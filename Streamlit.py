661f13395748aa85a394cb40fc2f278eb4d471a6
Monaflemit <108365633+Monaflemit@users.noreply.github.com>
2023-12-28 00:10:54 +0100

    Update Streamlit.py Suppr matplotlib
    Suppression de matplotlib

--- Streamlit.py
+++ Streamlit.py
@@ -1,5 +1,4 @@
 import pandas as pd  #!pip install --ignore-installed Pygments
-import matplotlib.pyplot as plt
 #streamlit run Streamlit.py
 url = "https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics/exports/csv?lang=fr&refine=annee_universitaire%3A%222022-23%22&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B"
 df = pd.read_csv(url,sep=';')
@@ -10,7 +9,7 @@ import streamlit as st
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Ajouter un titre
-st.title("Pourcentages de femmes dans les principaux diplômes préparés dans l'Enseignement supérieur")
+st.title("Pourcentage de femmes dans les principaux diplômes préparés dans l'Enseignement supérieur")
 
 # Ajouter du texte
 st.write('Source : https://data.enseignementsup-recherche.gouv.fr/explore/dataset/fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics/information/')
@@ -23,15 +22,15 @@ def plot_graph1():
     df_grouped['% femmes'] = df_grouped['Dont femmes'] / (df_grouped['Dont femmes'] + df_grouped['Dont hommes']) * 100
     df_grouped = df_grouped.sort_values('% femmes', ascending=True)
 
-    plt.xticks(range(len(df_grouped['Grande discipline'])), df_grouped['Grande discipline'], rotation=90)
+    #plt.xticks(range(len(df_grouped['Grande discipline'])), df_grouped['Grande discipline'], rotation=90)
 
     # Créer un graphique en barres avec les pourcentages de femmes
-    plt.bar(range(len(df_grouped['Grande discipline'])), df_grouped['% femmes'])
+    st.bar_chart(df_grouped.set_index('Grande discipline')['% femmes'], use_container_width=True)
 
     # Ajouter des étiquettes pour les axes et le titre
-    plt.xlabel('Grande discipline')
-    plt.ylabel('Pourcentage de femmes')
-    plt.title('Pourcentage de femmes par grande discipline en 2022')
+    #st.set_xlabel('Grande discipline')
+    #st.set_ylabel('Pourcentage de femmes')
+    #st.set_title('Pourcentage de femmes par grande discipline en 2022')
 
     # Afficher le graphique
     st.pyplot()
@@ -52,18 +51,15 @@ def plot_graph2():
 
     df_grouped = df_grouped.sort_values('% femmes', ascending=True)
 
-    plt.xticks(range(len(df_grouped['Discipline'])), df_grouped['Discipline'],rotation=90)
-
-
-
+    #plt.xticks(range(len(df_grouped['Discipline'])), df_grouped['Discipline'],rotation=90)
 
     # Créer un graphique en barres avec les pourcentages de femmes
-    plt.bar(range(len(df_grouped['Discipline'])), df_grouped['% femmes'])
+    st.bar_chart(df_grouped.set_index('Discipline')['% femmes'], use_container_width=True)
 
     # Ajouter des étiquettes pour les axes et le titre
-    plt.xlabel('Discipline')
-    plt.ylabel('Pourcentage de femmes')
-    plt.title('Pourcentage de femmes par discipline en 2022')
+    #plt.xlabel('Discipline')
+    #plt.ylabel('Pourcentage de femmes')
+    #plt.title('Pourcentage de femmes par discipline en 2022')
     
     # Afficher le graphique
     st.pyplot()
@@ -90,18 +86,18 @@ def plot_graph3():
 
     df_grouped = df_grouped.sort_values('% femmes', ascending=True)
 
-    plt.xticks(range(len(df_grouped['Secteur disciplinaire'])), df_grouped['Secteur disciplinaire'],rotation=90)
+    #plt.xticks(range(len(df_grouped['Secteur disciplinaire'])), df_grouped['Secteur disciplinaire'],rotation=90)
 
 
 
 
     # Créer un graphique en barres avec les pourcentages de femmes
-    plt.bar(range(len(df_grouped['Secteur disciplinaire'])), df_grouped['% femmes'])
-
+    st.bar_chart(df_grouped.set_index('Secteur disciplinaire')['% femmes'], use_container_width=True)
+    
     # Ajouter des étiquettes pour les axes et le titre
-    plt.xlabel('Secteur disciplinaire')
-    plt.ylabel('Pourcentage de femmes')
-    plt.title('Pourcentage de femmes par secteur disciplinaire en 2022')
+    #plt.xlabel('Secteur disciplinaire')
+    #plt.ylabel('Pourcentage de femmes')
+    #plt.title('Pourcentage de femmes par secteur disciplinaire en 2022')
 
     # Afficher le graphique
     st.pyplot()
@@ -124,18 +120,15 @@ def plot_graph4():
 
     df_grouped = df_grouped.sort_values('% femmes', ascending=True)
 
-    plt.xticks(range(len(df_grouped['Diplôme'])), df_grouped['Diplôme'],rotation=90)
-
-
-
+    #plt.xticks(range(len(df_grouped['Diplôme'])), df_grouped['Diplôme'],rotation=90)
 
     # Créer un graphique en barres avec les pourcentages de femmes
-    plt.bar(range(len(df_grouped['Diplôme'])), df_grouped['% femmes'])
-
+    st.bar_chart(df_grouped.set_index('Diplôme')['% femmes'], use_container_width=True)
+    
     # Ajouter des étiquettes pour les axes et le titre
-    plt.xlabel('Diplôme')
-    plt.ylabel('Pourcentage de femmes')
-    plt.title('Pourcentage de femmes par Diplôme en 2022')
+    #plt.xlabel('Diplôme')
+    #plt.ylabel('Pourcentage de femmes')
+    #plt.title('Pourcentage de femmes par Diplôme en 2022')
 
     # Afficher le graphique
     st.pyplot()
@@ -144,5 +137,5 @@ def plot_graph4():
     df_display
     
 if col4.button('Pourcentage de femmes par Diplôme'):
-    plot_graph4()       
+    plot_graph4()         
     
